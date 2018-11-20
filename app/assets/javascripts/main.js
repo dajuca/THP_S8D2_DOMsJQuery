7ed08242eec1c9aaa0412af700a68782377129a0
Clem Baldy <clemoun@yahoo.fr>
2018-11-20 16:16:17 +0100

    dropdown fonctionnel

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  
+
+  //TAB POUR LES CATEGORY
+
   //fait disparaître les div sous chaque li (sauf première)
   $(".category-div").hide();
 
@@ -8,11 +12,30 @@ $(document).ready(function() {
 
 
   $(".category-item").click(function(){
-    //pour surligner le tab actif
+    //pour surligner le tab actif (et pas les autres)
     $(".category-item").removeClass('active')
     $(this).addClass('active')
 
-    $(".category-div").hide();
-    $(this).next().show();
+    //afficher le  contenu correspondant au tab
+    $(".category-div").fadeOut(500);
+    $(this).next().fadeIn(500);
   });
+
+  $(".navbar-nav")
+
+
+  //DROP-DOWN MENU
+  //En cas de clic, je rajoute la classe show à mon dropdown
+  $(document).click(function(){
+    $(".dropdown-contenu").removeClass('show');    
+  });
+
+  //En cas de clic, je rajoute/enlève la classe show à mon dropdown
+  $(".avatar").click(function(e){
+    e.stopPropagation(); //pour ne pas être affecté par la fonction précédente
+    $(".dropdown-contenu").toggleClass('show');    
+  });
+
+
+
 });
